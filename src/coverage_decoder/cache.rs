e08e325477864a81d62b689472ad93ad64d8d486
Marco Cavenati <Marco.Cavenati@eurecom.fr>
2026-03-02 10:10:26 +0100

    Make LinearCache more generic

--- src/coverage_decoder/cache.rs
+++ src/coverage_decoder/cache.rs
@@ -1,22 +1,28 @@
 use super::ProceedInstStopReason;
 
-pub type Cache = LinearCache;
+pub type Cache = LinearCache<0x10_000, InstCacheKey, InstCacheValue>;
 
 #[derive(Debug)]
-pub struct LinearCache {
-    inner: Box<[(InstCacheKey, InstCacheValue); Self::SIZE]>,
+pub struct LinearCache<const SIZE: usize, K, V>
+where
+    K: Copy + From<u64> + PartialEq,
+{
+    inner: Box<[(K, V); SIZE]>,
 }
 
-impl LinearCache {
-    const SIZE: usize = 0x10_000;
-    const MASK: usize = Self::SIZE - 1;
+impl<const SIZE: usize, K, V> LinearCache<SIZE, K, V>
+where
+    K: Copy + From<u64> + PartialEq,
+    u64: From<K>,
+{
+    const MASK: usize = SIZE - 1;
+    // todo: assert SIZE is pow of 2
 
     pub fn new() -> Self {
         let inner = unsafe {
-            let mut inner_uninit =
-                Box::<[(InstCacheKey, InstCacheValue); Self::SIZE]>::new_zeroed();
+            let mut inner_uninit = Box::<[(K, V); SIZE]>::new_zeroed();
             // The 1 makes sure that the entry won't match any key and the cache will act as empty
-            inner_uninit.assume_init_mut()[0].0 = InstCacheKey { from: 1 };
+            inner_uninit.assume_init_mut()[0].0 = K::from(1);
             inner_uninit.assume_init()
         };
 
@@ -24,33 +30,45 @@ impl LinearCache {
     }
 
     #[inline]
-    pub fn get(&self, key: &InstCacheKey) -> Option<&InstCacheValue> {
-        let entry = &self.inner[key.from as usize & Self::MASK];
-        if entry.0.from == key.from {
-            Some(&entry.1)
-        } else {
-            None
-        }
+    pub fn get(&self, key: K) -> Option<&V> {
+        let entry = &self.inner[u64::from(key) as usize & Self::MASK];
+        if entry.0 == key { Some(&entry.1) } else { None }
     }
 
-    pub fn insert(&mut self, key: InstCacheKey, value: InstCacheValue) {
-        let entry = &mut self.inner[key.from as usize & Self::MASK];
+    pub fn insert(&mut self, key: K, value: V) {
+        let entry = &mut self.inner[u64::from(key) as usize & Self::MASK];
         entry.0 = key;
         entry.1 = value;
     }
 }
 
-impl Default for LinearCache {
+impl<const SIZE: usize, K, V> Default for LinearCache<SIZE, K, V>
+where
+    K: Copy + From<u64> + PartialEq,
+    u64: From<K>,
+{
     fn default() -> Self {
         Self::new()
     }
 }
 
-#[derive(Debug, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq)]
 pub struct InstCacheKey {
     pub from: u64,
     // todo: cr3
     // vmcs: Option<Vmcs>,
+}
+
+impl From<u64> for InstCacheKey {
+    fn from(value: u64) -> Self {
+        Self { from: value }
+    }
+}
+
+impl From<InstCacheKey> for u64 {
+    fn from(value: InstCacheKey) -> Self {
+        value.from
+    }
 }
 
 #[derive(Debug)]
